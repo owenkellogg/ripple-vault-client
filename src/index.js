5c5776116216c4f54e3fab5313518df9cfdbc716
Matthew Fettig <matthew@ripple.com>
2014-04-22 10:51:42 -0700

    [FEATURE] Unlock and LoginAndUnlock

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 var AuthInfo   = require('./authinfo');
 var blobClient = require('./blob');
-var kdf        = require('./kdf');
+var crypt      = require('./crypt');
 
 function VaultClient(opts) {
   if (!opts) opts = {};  
@@ -8,10 +8,15 @@ function VaultClient(opts) {
   
   this.domain    = opts.domain || 'ripple.com';
   this.authInfo  = new AuthInfo;
+  this.infos     = {};
 };
 
 
-
+/*
+ * Login -
+ * authenticate and retrieve a decrypted blob using a ripple name and password
+ * 
+ */
 VaultClient.prototype.login = function(username, password, fn) {
   var self = this;
   
@@ -35,17 +40,20 @@ VaultClient.prototype.login = function(username, password, fn) {
     if ("string" !== typeof authInfo.blobvault) {
       return fn(new Error("No blobvault specified in the authinfo."));
     }
+    
                   
     //derive login keys
-    kdf.deriveRemotely(authInfo.pakdf, 'login', username.toLowerCase(), password, function(err, keys){
+    crypt.derive(authInfo.pakdf, 'login', username.toLowerCase(), password, function(err, keys){
       if (err) return fn(err);
       
       blobClient.get(authInfo.blobvault, keys.id, keys.crypt, function (err, blob) {
         if (err) return fn(err);
         
+        self.infos[keys.id] = authInfo;  //save for relogin
+        
         fn (null, {
-          blob     : blob,
-          keys     : keys 
+          blob : blob,
+          keys : keys 
         });
       });
     });
@@ -53,33 +61,86 @@ VaultClient.prototype.login = function(username, password, fn) {
 };
 
 
+/*
+ * Relogin -
+ * retreive and decrypt blob using id and crypt derived previously.
+ * 
+ */
 VaultClient.prototype.relogin = function(id, cryptKey, fn) { 
-
-  fn(null, {
-    blob: true
+  var authInfo = this.infos[id]; 
+  
+  if (!authInfo) return fn(new Error("Unable to find authInfo"));
+  
+  blobClient.get(authInfo.blobvault, id, cryptKey, function (err, blob) {
+    if (err) return fn(err);
+    
+    fn (null, {
+      blob : blob,
+    });
   });
-
 };
 
+
+/*
+ * Unlock - 
+ * decrypt the secret key using a username and password
+ */
 VaultClient.prototype.unlock = function(username, password, encryptSecret, fn) {
-
-  fn(null, {
-    wallet: {
-      secret: true
-    }
+  var self = this;
+  
+  self.authInfo.get(self.domain, username, function(err, authInfo){
+    if (err) return fn(err);
+    
+    //derive unlock key
+    crypt.derive(authInfo.pakdf, 'unlock', username.toLowerCase(), password, function(err, keys){
+      if (err) return fn(err);
+      
+      fn(null, {
+        keys   : keys,
+        secret : crypt.decrypt(keys.unlock, encryptSecret)
+      });      
+    }); 
   });
-
 };
 
+
+/*
+ * LoginAndUnlock
+ * retrieve the decrypted blob and secret key in one step using
+ * the username and password
+ * 
+ */
 VaultClient.prototype.loginAndUnlock = function(username, password, fn) {
-
-  fn(null, {
-    wallet: {
-      address: true,
-      secret: true
-    }
+  var self = this;
+  
+  this.login(username, password, function(err, resp){
+    if (err) return fn(err);
+    
+    if (!resp.blob || 
+        !resp.blob.data ||
+        !resp.blob.data.encrypted_secret) 
+      return (new Error("Unable to retrieve blob and secret."));
+    
+    if (!resp.keys) return (new Error("Unable to retrieve keys."));
+       
+    //get authInfo via id - would have been saved from login
+    var authInfo = self.infos[resp.keys.id]; 
+    if (!authInfo) return fn(new Error("Unable to find authInfo"));
+      
+    //derive unlock key
+    crypt.derive(authInfo.pakdf, 'unlock', username.toLowerCase(), password, function(err, keys){
+      if (err) return fn(err); 
+      fn(null, {
+        blob : resp.blob,
+        keys : {
+          id     : resp.keys.id,
+          crypt  : resp.keys.crypt,
+          unlock : keys.unlocked
+        },
+        secret: crypt.decrypt(keys.unlock, resp.blob.data.encrypted_secret)
+      });
+    });     
   });
-
 };
 
 module.exports = VaultClient;
